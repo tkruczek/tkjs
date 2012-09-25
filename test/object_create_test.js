a251e85e7ee5c8f89695bd26fbd91e5ecf6bb0a3
tkruczek <t.kruczek.pl@gmail.com>
2012-09-25 20:45:09 +0200

    Ant build task redone

--- test/object_create_test.js
+++ test/object_create_test.js
@@ -1,26 +1,27 @@
 /*global TestCase, assertFunction, assertObject, assertEquals, assertException */
 
-(function () {
-  "use strict";
-  TestCase('ObjectCreateTest', {
-    'test should be a function': function () {
-      assertFunction(Object.create);
-    },
-    'test should require and argument': function () {
-      assertException(function () {
-        Object.create();
-      }, 'TypeError');
-    },
-    'test should return an object': function () {
-      assertObject(Object.create({}));
-    },
-    'test should return object with specified prototype': function () {
-      var circle = { radius: 10 },
-          sphere = Object.create(circle);
-      
-      assertEquals(sphere.radius, circle.radius);
-      
-    }
-    
-  }); 
+(function() {
+	"use strict";
+	TestCase('ObjectCreateTest', {
+		'test should be a function' : function() {
+			assertFunction(Object.create);
+		},
+		'test should require and argument' : function() {
+			assertException(function() {
+				Object.create();
+			}, 'TypeError');
+		},
+		'test should return an object' : function() {
+			assertObject(Object.create({}));
+		},
+		'test should return object with specified prototype' : function() {
+			var circle = {
+				radius : 10
+			}, sphere = Object.create(circle);
+
+			assertEquals(sphere.radius, circle.radius);
+
+		}
+
+	});
 }());
